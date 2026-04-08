474f28cc43ab814e0f889da9c75170e1f309d654
dev778d <21pwcse203@uetpeshawar.edu.pk>
2026-04-08 22:52:23 +0500

    Remove unnecessary ragrative-app directory

--- ragrative-app/src/components/effects/CustomCursor.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-// Custom cursor disabled for cleaner look
-// The visible dots in the screenshot were from this component
-export function CustomCursor() {
-  return null;
-}
--- ragrative-app/src/components/effects/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./CustomCursor";
-export * from "./SmoothScroll";
-export * from "./MagneticButton";
-export * from "./FloatingContact";
